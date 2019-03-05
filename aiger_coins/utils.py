7af37bf40c5cfa539b0ee683ed8fe73c86200c97
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-03-05 13:02:59 -0800

    add space to make flake8 happy

--- aiger_coins/utils.py
+++ aiger_coins/utils.py
@@ -1,5 +1,6 @@
 import math
 
+
 # Python is deprecating fractions.gcd....
 # https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers/147539#147539  # noqa: E501
 def lcm(a, b):
