57fd5953f1a1335f9bd50d3569be41b3c9f2a65f
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-03-06 20:53:23 -0800

    support naming state and inputs + remove dead code

--- aiger_coins/utils.py
+++ aiger_coins/utils.py
@@ -12,30 +12,18 @@ def lcm(a, b):
 
 
 # Simplification of py-aiger-gridworld's chain.
-def chain(n, use_1hot=True):
+def chain(n, use_1hot=True, state_name='x', action='H'):
     bits = n + 1 if use_1hot else math.ceil(math.log2(n + 1))
     start = 1 if use_1hot else 0
 
-    x = atom(bits, 'x', signed=False)
-    forward = atom(1, 'H', signed=False)
+    x = atom(bits, state_name, signed=False)
+    forward = atom(1, action, signed=False)
 
     succ = x << 1 if use_1hot else x + 1
     x2 = ite(forward, succ, x)
-    return x2.aigbv['o', {x2.output: 'x'}].feedback(
-        inputs=['x'], outputs=['x'],
+    return x2.aigbv['o', {x2.output: state_name}].feedback(
+        inputs=[state_name], outputs=[state_name],
         initials=[start], keep_outputs=True, signed=False
-    )
-
-
-# Simplification of py-aiger-gridworld's chain.
-def chain1hot(n):
-    x = atom(n, 'x', signed=False)
-    forward = atom(1, 'H', signed=False)
-
-    x2 = ite(forward, x << 1, x)
-    return x2.aigbv['o', {x2.output: 'x'}].feedback(
-        inputs=['x'], outputs=['x'],
-        initials=[1], keep_outputs=True, signed=False
     )
 
 
