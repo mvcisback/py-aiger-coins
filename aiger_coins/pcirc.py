dc481a5ed930409cf58c455479a1abd3c6c39560
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-10-15 21:53:41 -0700

    Start working on arbitrary coin weight implemention.

--- aiger_coins/pcirc.py
+++ aiger_coins/pcirc.py
@@ -1,34 +1,81 @@
 from __future__ import annotations
 
+import heapq
 import random
+import uuid
 from fractions import Fraction
-from typing import Any, Callable, Union
+from functools import reduce
+from typing import Any, Callable, Union, Mapping, Tuple
 
+import aiger
+import aiger_bv as BV
 import aiger_discrete
 import attr
+import funcy as fn
 from aiger_discrete import FiniteFunc
 from aiger_discrete.discrete import project, TIMED_NAME
 from pyrsistent import pmap
 from pyrsistent.typing import PMap
 
 
-Probability = Union[float, Fraction]
-Distribution = Callable[[Any], Union[float, Fraction]]
+Prob = Union[float, Fraction]
+Distribution = Mapping[Any, Prob]
 
 
-def rejection_sample(name: str, func: FiniteFunc, dist: Distribution) -> Any:
-    size = func.imap[name].size
-    encoding = func.input_encodings.get(name, aiger_discrete.Encoding())
-    while True:
-        val = encoding.decode(random.getrandbits(size))
-        prob = dist(val)
+def coin_gadget(name: str,
+                dist: Distribution, 
+                func: FiniteFunc,
+                tree_encoding: bool = True) -> PCirc:
+    """Return a Probabilistic Circuit representing a distribution.
 
-        if prob == 1:
-            return val
-        elif prob == 0:
-            continue
-        elif random.random() <= dist(val):
-            return val
+    - chain encoding. O(|support|)
+    - tree encoding. O(log|support|)
+    """
+    # Transform support of dist to UnsignedBVExpr using encoding.
+    size = func.circ.imap[name].size
+    encode = func.input_encodings \
+                 .get(name, aiger_discrete.Encoding()) \
+                 .encode
+    dist = ((p, BV.uatom(size, encode(v))) for v, p in dist.items())
+    
+    # Create priority queue.
+    cost = len if tree_encoding else (lambda x: -len(x))
+    queue = [(cost(elem), elem) for elem in dist]
+
+    coins = []
+    while len(queue) > 1:
+        cost1, (weight1, expr1) = heapq.heappop(queue)
+        cost2, (weight2, expr2) = heapq.heappop(queue)
+
+        # Create new coin input.
+        coin = BV.uatom(1, None) 
+        bias = weight1 / (weight1 + weight2)
+        coins.append((fn.first(coin.inputs), bias))
+        
+        # Merge and add back to queue.
+        expr12 = BV.ite(coin, expr1, expr2)
+        cost12 = cost1 + cost2  # (-x + -y) == -(x + y).
+        weight12 = weight1 + weight2
+        heapq.heappush(queue, (cost12, (weight12, expr12)))
+    assert len(queue) == 1
+    _, (weight, expr) = queue[0]
+
+    expr = expr.bundle_inputs(order=[c for c, _ in coins]) \
+               .with_output(name)
+    assert len(expr.inputs) == 1
+    coins_id = fn.first(expr.inputs)
+    biases = tuple(bias for _, bias in coins)
+    return PCirc(expr.aigbv, coins_id=coins_id, coin_biases=biases)
+
+
+def coin_gadgets(dist_map: Mapping[str, Distribution],
+                 func: FiniteFunc,
+                 tree_encoding: bool = True) -> PCirc:
+    """Return a Probabilistic Circuit representing a product distribution."""
+    gadgets = (
+        coin_gadget(k, v, func, tree_encoding) for k, v in dist_map.items()
+    )
+    return reduce(PCirc.__or__, gadgets)
 
 
 def to_finite_func(circ) -> FiniteFunc:
@@ -37,19 +84,47 @@ def to_finite_func(circ) -> FiniteFunc:
     return aiger_discrete.from_aigbv(circ.aigbv)
 
 
+def merge_pcirc_coins(circ, left: PCirc, right: PCirc, coins_id: str):
+    if not left.has_coins and not right.has_coins:
+        biases = ()
+    elif left.has_coins and not right.has_coins:
+        circ <<= BV.uatom(left.num_coins, coins_id) \
+                   .with_output(left.coins_id).aigbv
+        biases = left.coin_biases
+    elif right.has_coins and not left.has_coins:
+        circ <<= BV.uatom(right.num_coins, coins_id) \
+                   .with_output(right.coins_id).aigbv
+        biases = right.coin_biases
+    else:
+        coins = BV.uatom(left.num_coins + right.num_coins, coins_id)
+        circ <<= coins[:left.num_coins].with_output(left.coins_id).aigbv
+        circ <<= coins[:right.num_coins].with_output(right.coins_id).aigbv
+        biases = tuple(left.coin_biases) + tuple(right.coin_biases)
+    return circ, biases
+
+
+def sample_coins(coin_biases: Sequence[float]) -> int:
+    """Return integer where bits bias towards 1 as described in coin_biases."""
+    result = 0
+    for i, bias in enumerate(coin_biases):
+        result |= int(random.random() < bias) << i
+    return result
+
+
 @attr.s(frozen=True, auto_attribs=True)
 class PCirc:
     """Wrapper around AIG representing a function with some random inputs."""
     circ: FiniteFunc = attr.ib(converter=to_finite_func)
-    dist_map: PMap[str, Probability] = attr.ib(converter=pmap, default=pmap())
+    coin_biases: Sequence[Prob]           # Bias of each coin flips.
+    coins_id: str = "##coins"             # Input reservered for coin flips.
 
-    def __attrs_post_init__(self):
-        """Make sure unneeded distributions are forgotten."""
-        dist_map = project(self.dist_map, self.circ.inputs)
-        object.__setattr__(self, "dist_map", dist_map)
+    # TODO: validate that coin biases matches length of inputs.
 
     @property
-    def inputs(self): return self.circ.inputs - set(self.dist_map.keys())
+    def has_coins(self): return len(self.coin_biases) > 0
+
+    @property
+    def inputs(self): return self.circ.inputs - {self.coins_id}
 
     @property
     def outputs(self): return self.circ.outputs
@@ -66,52 +141,47 @@ class PCirc:
     @property
     def aigbv(self): return self.circ.aigbv
 
+    @property
+    def num_coins(self): return len(self.coin_biases)
+
     def assume(self, aigbv_like) -> PCirc:
         """Return Probabilistic Circuit with new assumption over the inputs."""
         return attr.evolve(self, circ=self.circ.assume(aigbv_like))
 
+    def with_coins_id(self, name=None):
+        if name is None:
+            name = str(uuid.uuid1())
+        circ = self.circ
+        if self.has_coins:
+            circ = self.circ['i', {self.coins_id: name}]
+        return attr.evolve(self, circ=circ, coins_id=name)
+
     def __rshift__(self, other) -> PCirc:
-        circ = self.circ >> canon(other).circ
-        return PCirc(circ, other.dist_map + self.dist_map)
+        other = canon(other)
+        circ = self.circ >> other.circ
+        circ, biases = merge_pcirc_coins(circ, self, other, self.coins_id)
+        return PCirc(circ, coins_id=self.coins_id, coin_biases=biases)
 
     def __lshift__(self, other) -> PCirc:
         return canon(other) >> self
 
     def __or__(self, other) -> PCirc:
-        if set(self.dist_map.keys()) & set(self.dist_map.keys()):
-            raise ValueError("Circuits have conflicting dist_maps.")
-        circ = self.circ | canon(other).circ
-        return PCirc(circ, self.dist_map + other.dist_map)
+        other = canon(other)
+        circ = self.circ | other.circ
+        circ, biases = merge_pcirc_coins(circ, self, other, self.coins_id)
+        return PCirc(circ, coins_id=self.coins_id, coin_biases=biases)
 
-    def __call__(self, inputs, latches=None, max_tries=1_000):
+    def __call__(self, inputs, latches=None):
         inputs = dict(inputs)
-        for count in range(max_tries + 1):
-            inputs.update({
-                k: rejection_sample(k, self.circ, dist)
-                for k, dist in self.dist_map.items()
-            })
-
-            try:
-                return self.circ(inputs=inputs, latches=latches)
-            except ValueError:  # Invalid input selected.
-                # TODO: build BDD to uniformly sample.
-                if count <= max_tries:
-                    continue
-                raise RuntimeError("Rejection Sampling failed!")
+        if self.has_coins:
+            inputs[self.coins_id] = sample_coins(self.coin_biases)
+        return self.circ(inputs=inputs, latches=latches)
 
     def __getitem__(self, others) -> PCirc:
         circ = self.circ[others]
         kind, relabels = others
-
-        kwargs = {}
-        if kind == 'i':
-            evolver = self.dist_map.evolver()
-            for old, new in relabels.items():
-                if old not in self.dist_map:
-                    continue
-                evolver[new] = self.dist_map[old]
-                del evolver[old]
-            kwargs['dist_map'] = evolver.persistent()
+        if (kind == 'i') and (self.coins_id in relabels):
+            raise ValueError("Use with_coins_id to relabel coins.")
 
         return attr.evolve(self, circ=circ, **kwargs)
 
@@ -126,40 +196,65 @@ class PCirc:
                init=True,
                omit_latches=True,
                only_last_outputs=False) -> PCirc:
+        # Unroll underlying circuit.
         circ = self.circ.unroll(
-            horizon=horizon,
-            init=init,
-            omit_latches=omit_latches,
+            horizon=horizon, init=init, omit_latches=omit_latches, 
             only_last_outputs=only_last_outputs,
         )
-        dist_map = {}
-        for timed_name in circ.inputs:
-            # Should always match because of unrolling.
-            name = TIMED_NAME.match(timed_name).groups()[0]
-            if name not in self.dist_map:
-                continue
-            dist_map[timed_name] = self.dist_map[name]
 
-        return attr.evolve(self, circ=circ, dist_map=dist_map)
+        if not self.has_coins:
+            return attr.evolve(self, circ=circ)
+
+        # Merge timed coin sequences into a single input.
+        coins = BV.uatom(self.num_coins * horizon, coins_id)
+        for time in range(horizon):
+            name = f"{circ.coins_id}##time_{time}"
+            assert name in circ.inputs
+
+            start = time * num_coins
+            end = start + num_coins
+            circ <<= coins[start:end].with_output(name).aigbv
+        biases = self.coin_biases * horizon
+
+        return PCirc(circ, coins_id=self.coins_id, coins_biases=biases)
 
     simulator = aiger_discrete.FiniteFunc.simulator
     simulate = aiger_discrete.FiniteFunc.simulate
 
-    def use_faircoins(self) -> PCirc:
-        # TODO: return PCirc where
-        # 1. every random variable's encoding is i.d.
-        # 2. every random distribution is over fair coins.
-        pass
 
-    def with_distmap(self, dist_map) -> PCirc:
-        """Update the distributions over the inputs."""
-        return attr.evolve(self, dist_map=self.dist_map + pmap(dist_map))
+def pcirc(func,
+          dist_map: Optional[Mapping[str, Distribution]] = None,
+          tree_encoding: bool=True) -> PCirc:
+    """Lift Discrete Function to a probilistic circuit."""
+    func = to_finite_func(func)
+    if dist_map is None:
+        return PCirc(circ=func)
+
+    gadgets = coin_gadgets(dist_map, func, tree_encoding)
+    return gadgets >> func
+
+
+def die(biases, name=None, tree_encoding=True):
+    """Model an n-sided dice with weights given in biases.
+
+    biases[i] = un-normalized probability of outputting i.
+    """
+    expr = BV.uatom(len(biases), name)
+    if name is None:
+        name = fn.first(expr.inputs)
+    func = to_finite_func(expr.with_output(name))
+    dist_map = {name: dict(enumerate(biases))}
+    return pcirc(func, dist_map=dist_map, tree_encoding=tree_encoding)
+
+
+def coin(bias, name=None):
+    return die((1 - bias, bias), name=name)
 
 
 def canon(circ) -> PCirc:
-    if isinstance(circ, PCirc):
-        return circ
-    return PCirc(circ)  # Assume aigbv like.
+    if not isinstance(circ, PCirc):
+        circ = PCirc(circ, coin_biases=())
+    return circ.with_coins_id()
 
 
-__all__ = ['PCirc']
+__all__ = ['PCirc', 'pcirc']
--- tests/test_pcirc.py
+++ tests/test_pcirc.py
@@ -1,86 +1,59 @@
+from fractions import Fraction
+
 import aiger_bv as BV
+import aiger_discrete
+from bidict import bidict
 
-import aiger_coins as C
-
-
-def test_pcirc_smoke():
-    x = BV.uatom(3, 'x')
-    y = BV.uatom(3, 'y')
-    z = (x + y).with_output('z')
-
-    pcirc = C.PCirc(circ=z, dist_map={'y': lambda _: 1/3}) \
-             .assume(y <= 2)
-
-    rvar = C.RandomVarCirc(pcirc)
-
-    # Warning. May be flaky.
-    for i in range(3):
-        assert 3 <= rvar({'x': 3}) <= 5
+from aiger_coins.pcirc import coin, die, pcirc, PCirc
 
 
-def test_pcirc_relabel():
-    x = BV.uatom(3, 'x')
-    pcirc = C.PCirc(circ=x, dist_map={'x': lambda _: 1/3})
-    pcirc2 = pcirc['i', {'x': 'y'}]
-    assert pcirc2.inputs == set()
-    assert pcirc2.circ.inputs == {'y'}
-    assert pcirc2.dist_map['y'](0) == 1/3
+def test_coin():
+    my_coin = coin(0.3, name='x')
+    assert my_coin.inputs == set()
+    assert my_coin.outputs == {'x'}
+    assert my_coin.coin_biases == (0.7,)
 
 
-def test_seq_compose():
-    x = BV.uatom(3, 'x').with_output('y')
-    y = BV.uatom(3, 'y').with_output('y')
-    pcirc = C.PCirc(circ=y)
-    pcirc2 = C.PCirc(circ=x, dist_map={'x': lambda _: 1/3}) >> pcirc
-    pcirc3 = pcirc << C.PCirc(circ=x, dist_map={'x': lambda _: 1/3})
+def test_die():
+    my_die = die((
+        Fraction(1, 6),
+        Fraction(1, 3),
+        Fraction(1, 2),
+    ), name='x')
+    assert my_die.inputs == set()
+    assert my_die.outputs == {'x'}
+    assert my_die.num_coins == 2
+    assert my_die.coin_biases == (Fraction(1, 3), Fraction(1, 2))
 
-    assert pcirc2.outputs == pcirc3.outputs == {'y'}
-    assert pcirc2.inputs == pcirc3.inputs == set()
-    assert pcirc2.dist_map['x'](0) == pcirc3.dist_map['x'](0) == 1/3
+    assert my_die.circ({my_die.coins_id: 0b00})[0]['x'] == 1
+    assert my_die.circ({my_die.coins_id: 0b01})[0]['x'] == 0
+    assert my_die.circ({my_die.coins_id: 0b10})[0]['x'] == 2
+    assert my_die.circ({my_die.coins_id: 0b11})[0]['x'] == 2
 
-    assert 0 <= pcirc2({})[0]['y'] <= 7
-
-    pcirc4 = C.PCirc(circ=(x + 1).with_output('y')) >> pcirc
-    assert pcirc4({'x': 0})[0] == {'y': 1}
-
-
-def test_par_compose():
-    x = BV.uatom(3, 'x').with_output('x')
-    y = BV.uatom(3, 'y').with_output('y')
-
-    pcirc_x = C.PCirc(circ=x)
-    pcirc_y = C.PCirc(circ=y, dist_map={'y': lambda _: 1/3})
-
-    pcirc_xy = pcirc_x | pcirc_y
-    assert pcirc_xy.inputs == {'x'}
-    assert pcirc_xy.outputs == {'x', 'y'}
-    assert pcirc_xy.dist_map['y'](0) == 1/3
+    assert (1 - my_die.coin_biases[0]) * (1 -my_die.coin_biases[1]) == \
+        Fraction(1, 3)
+    assert my_die.coin_biases[0] * (1 -my_die.coin_biases[1]) == Fraction(1, 6)
+    assert my_die.coin_biases[1] == Fraction(1, 2)
 
 
-def test_loopback_unroll():
-    x = BV.uatom(3, 'x')
-    y = BV.uatom(3, 'y')
-    adder = (x + y).with_output('z')
+def test_pcirc():
+    # Create encoder/decoder for dice.
+    lookup = bidict({0: '⚀', 1: '⚁', 2: '⚂', 3: '⚃'})
+    encoder = aiger_discrete.Encoding(decode=lookup.get, encode=lookup.inv.get)
 
-    pcirc = C.PCirc(adder, dist_map={'y': lambda _: 1/3}) \
-             .assume((y > 0) & (y < 4))
+    # Represent dice with a 2 bit vector.
+    expr1 = BV.uatom(2, '🎲') 
 
-    pcirc2 = pcirc.loopback({
-        'input': 'x',
-        'output': 'z',
-        'init': 4,
-        'keep_output': True,
+    # Add encoded dice to x. Because why not.
+    expr2 = BV.uatom(2, 'x') + expr1
+    func = aiger_discrete.from_aigbv(
+        expr2.aigbv, input_encodings={'🎲': encoder}
+    )
+
+    # Create distribution over bits.
+    circ = pcirc(func, dist_map={
+        '🎲': {'⚀': 1, '⚁': 2, '⚂': 1/3},
     })
 
-    assert pcirc2.inputs == set()
-    assert pcirc2.outputs == {'z'}
-    assert len(pcirc2.latches) == 1
-    assert 4 < pcirc2({})[0]['z'] < 8
-
-    pcirc3 = pcirc2.unroll(3)
-    assert pcirc3.inputs == set()
-    assert pcirc3.outputs == {'z##time_1', 'z##time_2', 'z##time_3'}
-
-    pcirc4 = pcirc2.unroll(3, only_last_outputs=True)
-    assert pcirc4.outputs == {'z##time_3'}
-    pcirc4({})  # Could technically be any value due to roll back.
+    assert circ.inputs == {'x'}
+    assert circ.outputs == {expr2.output}
