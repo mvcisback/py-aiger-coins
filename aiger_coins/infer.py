94b85160509ed0751863cacb3c0d7bb1c67aca29
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-11-11 18:48:39 -0800

    Support computing probability of predicate with coins.

---
+++ aiger_coins/infer.py
@@ -0,0 +1,67 @@
+from functools import reduce
+
+import aiger_bv as BV
+
+import funcy as fn
+import networkx as nx
+import numpy as np
+
+
+
+FALSE = 0b01
+TRUE = 0b10
+
+
+def onehot_gadget(circ):
+    output = fn.first(circ.outputs)
+    sat = BV.uatom(1, output)
+    false, true = BV.uatom(2, FALSE), BV.uatom(2, TRUE)
+    return BV.ite(sat, true, false).aigbv
+
+
+def prob(circ, *, log=False, manager=None):
+    from aiger_discrete.mdd import to_mdd
+    from mdd.nx import to_nx
+
+    if circ.inputs:
+        raise ValueError("All inputs must be randomized.")
+
+    if len(circ.outputs) != 1:
+        raise ValueError("Only support querying probability of single output.")
+
+
+    # Make coins different variables for MDD.
+    coins = (BV.uatom(1, f"coin_{i}") for i in range(circ.num_coins))    
+    coin_blaster = reduce(lambda x, y: x.concat(y), coins)
+    coin_blaster = coin_blaster.with_output(circ.coins_id) \
+                               .aigbv
+
+    #            Seperate coins          MDD expects 1-hot output
+    query = (circ.circ << coin_blaster) >> onehot_gadget(circ)
+
+    # View graph of MDD as circuit over LogSumExp    
+    graph = to_nx(to_mdd(query))
+    biases = {f"coin_{i}": bias for i, bias in enumerate(circ.coin_biases)}
+
+    lprobs = {}
+    for node in nx.topological_sort(graph.reverse()):
+        val = graph.nodes[node]['label']
+        if val == TRUE:
+            lprobs[node] = 0
+        elif val == FALSE:
+            lprobs[node] = -float('inf')
+        else:
+            left, right = graph.neighbors(node)
+            # Swap if polarity switched.
+            if graph.edges[node, right]['label']({val: True})[0]:
+                right, left = left, right
+
+            bias = biases[val]
+            log_biases = np.log(np.array([bias, 1 - bias]))
+            kid_lprobs = np.array([lprobs[left], lprobs[right]])
+
+            # Compute average likelihood in log scale.
+            lprobs[node] = np.logaddexp(*(log_biases + kid_lprobs))
+    
+    result = lprobs[node]
+    return result if log else np.exp(result)
---
+++ tests/test_infer.py
@@ -0,0 +1,30 @@
+import pytest
+
+import aiger_bv as BV
+import aiger_discrete
+import funcy as fn
+from bidict import bidict
+
+import aiger_coins as C
+from aiger_coins.infer import prob
+
+
+def test_infer():
+    # Create encoder/decoder for dice.
+    lookup = bidict({0: '⚀', 1: '⚁', 2: '⚂', 3: '⚃'})
+    encoder = aiger_discrete.Encoding(decode=lookup.get, encode=lookup.inv.get)
+
+    # Represent dice with a 2 bit vector.
+    expr1 = BV.uatom(2, '🎲') == encoder.encode('⚀')
+
+    func = aiger_discrete.from_aigbv(
+        expr1.aigbv, input_encodings={'🎲': encoder}
+    )
+    # Create distribution over bits.
+    circ = C.pcirc(func) \
+            .randomize({'🎲': {'⚀': 1/6, '⚁': 2/6, '⚂': 3/6}})
+
+    assert circ.inputs == set()
+    assert circ.outputs == {expr1.output}
+    assert circ.coin_biases == (1/3, 1/2)
+    assert prob(circ) == pytest.approx(1/6)
