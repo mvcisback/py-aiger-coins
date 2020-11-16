22558c9e3224d95e32254303428232a82dcbdd52
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-11-15 16:43:35 -0800

    Added support for finding coins to match i/o

--- aiger_coins/infer.py
+++ aiger_coins/infer.py
@@ -1,7 +1,6 @@
 from functools import reduce
 
 import aiger_bv as BV
-
 import funcy as fn
 import networkx as nx
 import numpy as np
@@ -63,3 +62,81 @@ def prob(circ, *, log=False, manager=None):
 
     result = lprobs[node]
     return result if log else np.exp(result)
+
+
+def coins_preimage(pcirc, *,
+                   inputs=None,
+                   outputs=None,
+                   latchins=None,
+                   latchouts=None) -> BV.UnsignedBVExpr:
+    if not pcirc.has_coins:
+        return BV.uatom(1, 1)
+
+    # Default IO.
+    if inputs is None:
+        inputs = {}
+    if outputs is None:
+        outputs = {}
+    if latchouts is None:
+        latchouts = {}
+    if latchins is None:
+        latchins = pcirc.latch2init
+
+    # Check interface.
+    assert set(inputs) == pcirc.inputs
+    assert set(outputs) == pcirc.outputs
+    assert set(latchouts) == pcirc.latches
+    assert set(latchins) == pcirc.latches
+
+    # Encode IO.
+    mappings = [
+        (inputs, pcirc.circ.input_encodings),
+        (outputs, pcirc.circ.output_encodings),
+    ]
+    for val_map, enc_map in mappings:
+        val_map.update({k: e.encode(val_map[k]) for k, e in enc_map.items()})
+
+    # Cutlatches and add to i/o.
+    circ, relabels = pcirc.aigbv.cutlatches()
+    relabels = {k: v for k, (v, _) in relabels.items()}  # Drop init info.
+    inputs.update({relabels[k]: v for k, v in latchins.items()})
+    outputs.update({relabels[k]: v for k, v in latchouts.items()})
+
+    # Fix inputs.
+    srcs = (
+        BV.uatom(circ.imap[k].size, v).with_output(k).aigbv
+        for k, v in inputs.items()
+    )
+    circ <<= reduce(lambda x, y: x | y, srcs)
+    assert len(circ.inputs) == int(pcirc.has_coins)
+
+    # Test for outputs.
+    omap = circ.omap
+    tests = (BV.uatom(omap[k].size, k) == v for k, v in outputs.items())
+    circ >>= reduce(lambda x, y: x & y, tests).aigbv
+    assert len(circ.outputs - {pcirc.circ.valid_id}) == bool(circ.outputs)
+
+    # Test for valid.
+    if len(circ.outputs) == 2:  # Force valid.
+        left, right = (BV.uatom(1, o) for o in circ.outputs)
+        circ >>= (left & right).aigbv
+
+    assert len(circ.outputs) == 1
+    return BV.UnsignedBVExpr(circ)
+
+
+def find_coins(pcirc, *,
+               inputs=None,
+               outputs=None,
+               latchins=None,
+               latchouts=None):
+    from aiger_sat.sat_bv import solve
+    expr = coins_preimage(
+        pcirc, inputs=inputs, outputs=outputs,
+        latchins=latchins, latchouts=latchouts
+    )
+    model = solve(expr)
+    return {k: BV.decode_int(v, signed=False) for k, v in model.items()}
+
+
+__all__ = ['prob', 'coins_preimage', 'find_coins']
--- tests/test_infer.py
+++ tests/test_infer.py
@@ -5,7 +5,7 @@ import aiger_discrete
 from bidict import bidict
 
 import aiger_coins as C
-from aiger_coins.infer import prob
+from aiger_coins.infer import prob, find_coins
 
 
 def test_infer():
@@ -27,3 +27,32 @@ def test_infer():
     assert circ.outputs == {expr1.output}
     assert circ.coin_biases == (1/3, 1/2)
     assert prob(circ) == pytest.approx(1/6)
+
+
+def test_find_coins():
+    # Create encoder/decoder for dice.
+    lookup = bidict({0: '⚀', 1: '⚁', 2: '⚂', 3: '⚃'})
+    encoder = aiger_discrete.Encoding(decode=lookup.get, encode=lookup.inv.get)
+
+    expr1 = BV.uatom(2, '🎲') + BV.uatom(2, 'x') + BV.uatom(2, 'z')
+
+    circ = expr1.with_output('y') \
+                .aigbv \
+                .loopback({'input': 'x', 'output': 'y', 'keep_output': True})
+
+    func = aiger_discrete.from_aigbv(circ, input_encodings={'🎲': encoder})
+
+    # Create distribution over bits.
+    pcirc = C.pcirc(func) \
+             .randomize({'z': {0: 1/6, 1: 2/6, 2: 3/6}}) \
+
+    model = find_coins(
+        pcirc,
+        inputs={'🎲': '⚁'},
+        outputs={'y': 2},
+        latchouts={'x': 2}
+    )
+    assert model
+    len(model) == 1
+    model.update({'🎲': '⚁'})
+    assert pcirc.circ(model) == ({'y': 2}, {'x': (False, True)})
